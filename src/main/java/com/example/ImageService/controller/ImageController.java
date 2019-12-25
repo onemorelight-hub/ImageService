623777dd8dc9a13907091bc8a5d234d4e9149db9
Anjan <contact.onemorelight@gmail.com>
2019-12-25 18:32:49 +0530

    Initial commit

---
+++ src/main/java/com/example/ImageService/controller/ImageController.java
@@ -0,0 +1,40 @@
+package com.example.ImageService.controller;
+
+import java.awt.PageAttributes.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.ImageService.exception.StorageException;
+import com.example.ImageService.service.StorageService;
+
+@Controller
+public class ImageController {
+
+	@Autowired
+	private StorageService storageService;
+	
+	@RequestMapping(value = "/doUpload", method = RequestMethod.POST, consumes =  {"multipart/form-data"})
+	public String upload(@RequestParam("file") MultipartFile file) {
+
+        storageService.uploadFile(file);
+
+        return "done";
+       // return "redirect:/success.html";
+    }
+	
+	@ExceptionHandler(StorageException.class)
+	public String handleStorageFileNotFound(StorageException e) {
+		return "redirect:/failure.html";
+	}
+}
---
+++ src/main/java/com/example/ImageService/service/StorageService.java
@@ -0,0 +1,36 @@
+package com.example.ImageService.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.ImageService.exception.StorageException;
+
+@Service
+public class StorageService {
+
+	@Value("${upload.path}")
+    private String path;
+
+    public void uploadFile(MultipartFile file) {
+
+        if (file.isEmpty()) {
+            throw new StorageException("Failed to store empty file");
+        }
+        try {
+            String fileName = file.getOriginalFilename();
+            InputStream is = file.getInputStream();
+            Files.copy(is, Paths.get(path + fileName),StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            String msg = String.format("Failed to store file", file.getName());
+            throw new StorageException(msg, e);
+        }
+
+    }
+}
